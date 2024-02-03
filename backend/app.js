a4cfcc5225088c1b708a696182fe535eb6943144
ksokolovsky <36239110+ksokolovsky@users.noreply.github.com>
2024-02-03 17:13:16 +0100

    Автотесты: 5

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ const errorLogger = expressWinston.errorLogger({
   ),
 });
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(requestLogger); // req logging
 
