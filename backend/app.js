9bc2c0f8fccfd7d8c7d83458330c2860e8f972cf
ksokolovsky <36239110+ksokolovsky@users.noreply.github.com>
2024-02-06 12:29:24 +0100

    Dobavil regexp v schema

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ const allowedCors = [
   'https://localhost:3000',
 ];
 
-console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV, 'env node_env - v app');
 
 app.use(cors());
 
--- backend/config.js
+++ backend/config.js
@@ -2,3 +2,4 @@ require('dotenv').config();
 
 const jwtSecret = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret';
 module.exports = { jwtSecret };
+
